d49e8c3b4d80b39c9aff77f62f17800280f97bec
Rainy <939029835@qq.com>
2022-03-10 20:54:37 +0800

    Update Runtime.hx

--- src/script/core/Runtime.hx
+++ src/script/core/Runtime.hx
@@ -6,15 +6,19 @@ class Runtime {
 	 * @param script 
 	 */
 	public static function run(script:IScript):Void {
+		// 开始循序执行script
 		if (script.scriptIndex < script.scripts.length) {
 			var runScript = script.scripts[script.scriptIndex];
 			runScript.onUpdate();
+			// 如果循序执行成功，则进入下一个
 			if (runScript.state == 0) {
 				script.scriptIndex++;
 				var runScript = script.scripts[script.scriptIndex];
 				if (runScript != null)
 					runScript.bindDisplay(runScript.display);
 				run(script);
+			} else if (runScript.state > 0) {
+				throw "Error state:" + runScript.state;
 			}
 		}
 	};
