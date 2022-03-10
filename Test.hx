cba6e4f0c258039290ca452a6650e5ee838809c3
Rainy <939029835@qq.com>
2022-03-10 20:53:13 +0800

    修复程序

--- Test.hx
+++ Test.hx
@@ -1,4 +1,4 @@
-import script.move.Move;
+
 
 class Test {
 	static function main() {
@@ -8,8 +8,6 @@ class Test {
 			x: 0,
 			y: 0
 		};
-		pupil.addScript(obj, new Move(200, 200));
-		pupil.addScript(obj, new Move(-100, 200));
 		pupil.start();
 		trace(pupil);
 		trace("运行结果：", obj);
--- src/Pupil.hx
+++ src/Pupil.hx
@@ -1,7 +1,8 @@
+#if zygame
 import zygame.utils.FrameEngine;
+#end
 import script.core.Runtime;
 import script.core.Script;
-import script.core.IScript;
 
 /**
  * 参考儿童编程，可以自定义组件，使组件按循序执行
@@ -11,6 +12,8 @@ class Pupil extends Script {
 	 * 开始执行
 	 */
 	public function start():Void {
+		if (this.scripts.length == 0)
+			return;
 		this.scriptIndex = 0;
 		this.scripts[0].bindDisplay(this.scripts[0].display);
 		#if zygame
